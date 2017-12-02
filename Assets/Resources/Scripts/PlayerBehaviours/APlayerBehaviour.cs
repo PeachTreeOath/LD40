763c0df4eb789610c18d64756465f49515e4e4cd
Tom <tomrogers917@gmail.com>
2017-12-02 12:18:06 -0700

    Starting the framework for behaviours

---
+++ Assets/Resources/Scripts/PlayerBehaviours/APlayerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A Base representation for what a players behaviour is. 
+/// all behaviour extend this class and their implementation defines the execution
+/// of the action button.
+/// </summary>
+public abstract class APlayerBehaviour  {
+
+    /// <summary>
+    /// Execute the player state specific action.
+    /// </summary>
+    public abstract void ExecuteBehaviourAction();
+}
---
+++ Assets/Resources/Scripts/PlayerBehaviours/JockStateBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JockStateBehaviour : APlayerBehaviour
+{
+    public override void ExecuteBehaviourAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
