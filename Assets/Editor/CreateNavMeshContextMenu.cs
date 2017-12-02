1900a5ed469b1ad726ca17447ea3166842974bc8
mjeffery <michael.jeffery@gmail.com>
2017-12-02 15:03:19 -0500

    collect the collision data for tilemaps from a grid

---
+++ Assets/Editor/CreateNavMeshContextMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+using System;
+
+public class CreateNavMeshContextMenu {
+
+    const int BIG_VALUE = 1000;
+    const int SMALL_VALUE = -1000;  //MAX/MIN constants 
+ 
+    [MenuItem("CONTEXT/Grid/Create Nav Waypoints")]
+    private static void CreateNavWaypoints(MenuCommand menuCommand) {
+        Debug.Log("Creating Nav Waypoints");
+
+        var grid = menuCommand.context as Grid;
+        var tilemaps = grid.GetComponentsInChildren<Tilemap>();
+
+        var bounds = GetTotalBounds(tilemaps);
+
+        NavTilemap navMap = new NavTilemap(bounds);
+
+        UpdateTileCollision(navMap, tilemaps);
+       
+
+    }
+
+    private static BoundsInt GetTotalBounds(Tilemap[] tilemaps) {
+        BoundsInt bounds = new BoundsInt();
+        bool doneFirst = false;
+
+        foreach (Tilemap tilemap in tilemaps) {
+            if(!doneFirst) {
+                bounds = tilemap.cellBounds;
+                doneFirst = true;
+            } else {
+                BoundsInt cellBounds = tilemap.cellBounds;
+
+                bounds.xMin = Math.Min(bounds.xMin, cellBounds.xMin);
+                bounds.yMin = Math.Min(bounds.yMin, cellBounds.yMin);
+                bounds.xMax = Math.Max(bounds.xMax, cellBounds.xMax);
+                bounds.yMax = Math.Max(bounds.yMax, cellBounds.yMax);
+            }
+        }
+
+        return bounds;
+    }
+
+    private static void UpdateTileCollision(NavTilemap navMap, Tilemap[] tilemaps) {
+        foreach(Tilemap tilemap in tilemaps) {
+            if (!HasTileCollision(tilemap)) continue;
+
+            for(int x = tilemap.cellBounds.xMin; x < tilemap.cellBounds.xMax; x++) {
+                for (int y = tilemap.cellBounds.yMin; y < tilemap.cellBounds.yMax; y++) {
+                    var tile = tilemap.GetTile(new Vector3Int(x, y, 0));
+                    if (tile) {
+                        navMap.GetTile(x, y).solid = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private static bool HasTileCollision(Component component) {
+        return component.gameObject.GetComponent<TilemapCollider2D>() != null;
+    }
+}
---
+++ Assets/Resources/Scripts/NavTilemap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavTilemap {
+
+    public class NavTile {
+        public bool solid = false;
+    }
+
+    private BoundsInt navBounds;
+    private NavTile[,] tiles;
+
+    public BoundsInt cellBounds { get { return navBounds; } }
+
+    public NavTilemap(BoundsInt bounds) {
+        navBounds = bounds;
+        tiles = new NavTile[bounds.size.x, bounds.size.y];
+
+        for(int x = 0; x < tiles.GetLength(0); x++) {
+            for(int y = 0; y < tiles.GetLength(1); y++) {
+                tiles[x, y] = new NavTile();
+            }
+        }
+    }
+
+    public NavTile GetTile(int x, int y) {
+        int navX = x - navBounds.xMin;
+        int navY = y - navBounds.yMin;
+
+        return tiles[navX, navY];
+    }
+
+    public NavTile GetTile(Vector3Int location) {
+        return GetTile(location.x, location.y);
+    }
+}
