e7dfd9e7e61964a911aa90ecb2b2a9820e05e3b1
PeachTreeOath <peachtreeoath@gmail.com>
2017-12-02 02:07:01 -0700

    Prefabbing scene

---
+++ Assets/Resources/Scripts/VolumeListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VolumeListener : MonoBehaviour {
+
+	public static float volumeLevel = 1;
+
+	// Use this for initialization
+	void Start () {
+       // gameObject.GetComponent<Slider>().value = AudioManager.instance.GetMusicVolume(); 
+    }
+	
+	public void VolumeChanged()
+	{
+		volumeLevel = gameObject.GetComponent<Slider>().value;
+		AudioManager.instance.UpdateMusicVolume();
+	}
+}
