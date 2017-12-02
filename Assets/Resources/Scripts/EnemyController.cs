540c3e4fa47a8f8b2264398180bec8659fc4efcc
Fangor <rfonger@gmail.com>
2017-12-02 16:02:47 -0700

    PersonalAffinity working

--- Assets/Resources/Scripts/EnemyController.cs
+++ Assets/Resources/Scripts/EnemyController.cs
@@ -6,6 +6,13 @@ public class EnemyController : MonoBehaviour {
 
     public CliqueEnum clique;
 
+    //Stores affinity per enemy so we cannot max affinity on one player.
+    public float personalAffinity;
+    public float personalAffinityMax;
+
+    public float incrementAffinitySpeed;
+    public float decrementAffinitySpeed;
+
     [HideInInspector]
     public Rigidbody2D rbody; // public so the states can easily access
 
@@ -46,12 +53,38 @@ public class EnemyController : MonoBehaviour {
         {
             if(clique == player.GetPlayerState())
             {
-                LevelManager.instance.incrementCurrentAffiliation(clique, state.GetIncrementAffiliationSpeed() * Time.deltaTime);
+                if (personalAffinity < personalAffinityMax)
+                {
+                    //This logic is so that we do not go above personal affinity max, and also so levelCliqueAffinity does not get
+                    //incremented any more than personal.
+                    float oldPersonalAffinity = personalAffinity;
+                    float newPersonalAffinity = Mathf.Min(personalAffinityMax, personalAffinity + incrementAffinitySpeed * Time.deltaTime);
+                    float deltaPersonalAffinity = newPersonalAffinity - oldPersonalAffinity;
+                    IncrementPersonalAffinity(deltaPersonalAffinity);
+                    LevelManager.instance.incrementCurrentAffinity(clique, deltaPersonalAffinity);
+                }
             }
             else
             {
-                LevelManager.instance.incrementCurrentAffiliation(clique, state.GetDecrementAffiliationSpeed() * Time.deltaTime);
+                float oldPersonalAffinity = personalAffinity;
+                float newPersonalAffinity = Mathf.Max(0, personalAffinity + decrementAffinitySpeed * Time.deltaTime);
+                float deltaPersonalAffinity = newPersonalAffinity - oldPersonalAffinity;
+                IncrementPersonalAffinity(deltaPersonalAffinity);
+                LevelManager.instance.incrementCurrentAffinity(clique, deltaPersonalAffinity);
             }
         }
+    }
+
+    public virtual float GetPersonalAffinity()
+    {
+        return personalAffinity;
+    }
+
+    public virtual void IncrementPersonalAffinity(float incrementAmount)
+    {
+        if (incrementAmount >= 0)
+            personalAffinity = Mathf.Min(personalAffinityMax, personalAffinity + incrementAmount);
+        else
+            personalAffinity = Mathf.Max(0, personalAffinity + incrementAmount);
     }
 }
