fd7b4ff8810551047ee3afd98b86222f5e7ff931
Fangor <rfonger@gmail.com>
2017-12-02 04:20:24 -0700

    Adding levelmanager to handle clique stats

---
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : Singleton<LevelManager> {
+
+    public float requiredAffiliationPerClique;
+    public float totalAffiliationPerClique;
+    public float defaultAffiliation;
+
+    private Dictionary<CliqueEnum, float> cliqueAffiliations = new Dictionary<CliqueEnum, float>();
+    private Canvas canvas;
+    private Text skaterCanvasValue;
+    private Text furryCanvasValue;
+    private Text jockCanvasValue;
+
+	// Use this for initialization
+	void Start () {
+        //Set default affiliation for all affiliations
+        foreach (CliqueEnum clickEnum in Enum.GetValues(typeof(CliqueEnum)))
+        {
+            cliqueAffiliations.Add(clickEnum, defaultAffiliation);
+        }
+        
+        //Get the canvas to use later
+        jockCanvasValue = GameObject.Find("Jocks Affinity Value").GetComponent<Text>();
+        skaterCanvasValue = GameObject.Find("Skaters Affinity Value").GetComponent<Text>();
+        furryCanvasValue = GameObject.Find("Furries Affinity Value").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateCanvas();
+	}
+
+    void UpdateCanvas()
+    {
+        jockCanvasValue.text = cliqueAffiliations[CliqueEnum.JOCK].ToString();
+        skaterCanvasValue.text = cliqueAffiliations[CliqueEnum.SK8R].ToString();
+        furryCanvasValue.text = cliqueAffiliations[CliqueEnum.FURBOI].ToString();
+    }
+}
