52cc9d3e5c0f83d10fd3885c0bbe5fb3fc532728
mjeffery <michael.jeffery@gmail.com>
2017-12-03 15:55:07 -0500

    added a 'hand joint' to the Furry

--- Assets/Resources/Scripts/EnemyState/FurryEnemyController.cs
+++ Assets/Resources/Scripts/EnemyState/FurryEnemyController.cs
@@ -6,6 +6,13 @@ public class FurryEnemyController : WandererEnemyController {
 
     const string HOLDING_HANDS_STATE = "holding hands";
 
+    private FixedJoint2D handJoint;
+
+    protected override void Start() {
+        base.Start();
+        handJoint = GetComponent<FixedJoint2D>();
+    }
+
     public override float GetStateSpeed()
     {
         return 1.0f;
@@ -24,12 +31,14 @@ public class FurryEnemyController : WandererEnemyController {
 
     public void StartHoldingHands() {
         StopMoveTo();
-        transform.SetParent(PlayerController.instance.gameObject.transform);
+        transform.SetParent(PlayerController.instance.transform);
+        handJoint.connectedBody = PlayerController.instance.GetComponent<Rigidbody2D>();
         state = HOLDING_HANDS_STATE;
     }
 
     public void StopHoldingHands() {
         transform.SetParent(null);
+        handJoint.connectedBody = null;
         StartWander(); 
     }
 
