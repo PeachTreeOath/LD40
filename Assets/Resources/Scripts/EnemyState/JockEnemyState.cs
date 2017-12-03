5332955c00eb444510b9abe1a024c58d429b5eed
mjeffery <michael.jeffery@gmail.com>
2017-12-02 21:29:17 -0500

    first pass at giving Jocks the wander state

--- Assets/Resources/Scripts/EnemyState/JockEnemyState.cs
+++ Assets/Resources/Scripts/EnemyState/JockEnemyState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JockEnemyState : AEnemyState {
+public class JockEnemyState : WandererEnemyState {
     
     /// <summary>
     /// The sprite the player takes on when changing to this state.
@@ -13,13 +13,7 @@ public class JockEnemyState : AEnemyState {
         return ResourceLoader.instance.jockSprite;
     }
 
-    /// <summary>
-    /// Updates against the enemy's transform
-    /// </summary>
-    /// <param name="transform"></param>
-    public override void DoUpdate(EnemyController enemy)
-    {
-        float step = GetStateSpeed() * Time.deltaTime;
-        enemy.rbody.MovePosition(Vector2.MoveTowards(enemy.transform.position, PlayerController.instance.transform.position, step));
+    public override float GetStateSpeed() {
+        return 3f;
     }
 }
