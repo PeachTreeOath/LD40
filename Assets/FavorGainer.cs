db84447dfa2d8adae078a4dbe183b182665a3144
PeachTreeOath <peachtreeoath@gmail.com>
2017-12-02 19:53:01 -0700

    Faction gain works again

---
+++ Assets/FavorGainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FavorGainer : MonoBehaviour
+{
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        EnemyController enemy = col.GetComponent<EnemyController>();
+        if(enemy != null)
+        {
+            enemy.ChangeFavor();
+        }
+    }
+
+}
