5512a9c356787f2231e51c6f867ad28494f9286a
PeachTreeOath <peachtreeoath@gmail.com>
2017-12-02 18:27:07 -0700

    Enemy spawners

--- Assets/Resources/Scripts/LevelManager.cs
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -32,10 +32,12 @@ public class LevelManager : Singleton<LevelManager> {
         jockCanvasValue = GameObject.Find("Jocks Affinity Value").GetComponent<Text>();
         skaterCanvasValue = GameObject.Find("Skaters Affinity Value").GetComponent<Text>();
         furryCanvasValue = GameObject.Find("Furries Affinity Value").GetComponent<Text>();
+
+        SpawnEnemies();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         UpdateCanvas();
 	}
 
@@ -62,6 +64,38 @@ public class LevelManager : Singleton<LevelManager> {
             cliqueAffiliations[clique] = Math.Min(totalAffiliationPerClique, cliqueAffiliations[clique] + incrementAmount);
         else
             cliqueAffiliations[clique] = Math.Max(0, cliqueAffiliations[clique] + incrementAmount);
+    }
+
+    private void SpawnEnemies()
+    {
+        int level = GlobalPersistentStats.instance.level;
+        GameObject levelObj = GameObject.Find("Level" + level);
+        Level lvl = levelObj.GetComponent<Level>();
+        Waypoint[] wps = GameObject.Find("Waypoints").GetComponentsInChildren<Waypoint>();
+
+        for(int i = 0; i < lvl.furryCount; i++)
+        {
+            Waypoint wp = wps[UnityEngine.Random.Range(0, wps.Length)];
+            Instantiate(ResourceLoader.instance.skaterPrefab, wp.transform.position, Quaternion.identity);
+        }
+
+        for (int i = 0; i < lvl.skaterCount; i++)
+        {
+            Waypoint wp = wps[UnityEngine.Random.Range(0, wps.Length)];
+            Instantiate(ResourceLoader.instance.skaterPrefab, wp.transform.position, Quaternion.identity);
+        }
+
+        for (int i = 0; i < lvl.footballerCount; i++)
+        {
+            Waypoint wp = wps[UnityEngine.Random.Range(0, wps.Length)];
+            Instantiate(ResourceLoader.instance.jockPrefab, wp.transform.position, Quaternion.identity);
+        }
+
+        for (int i = 0; i < lvl.hipsterCount; i++)
+        {
+            Waypoint wp = wps[UnityEngine.Random.Range(0, wps.Length)];
+            Instantiate(ResourceLoader.instance.skaterPrefab, wp.transform.position, Quaternion.identity);
+        }
     }
 
 }
