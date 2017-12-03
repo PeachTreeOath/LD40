b5a53e2df2227a7f162ab7c7138a867cb12dec27
mjeffery <michael.jeffery@gmail.com>
2017-12-03 00:29:30 -0500

    fixed the football to collide with walls

---
+++ Assets/Resources/Scripts/Football.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Football : MonoBehaviour {
+
+    private Rigidbody2D rbody;
+    private FootballPickup pickup;
+
+    void Start() {
+        rbody = GetComponent<Rigidbody2D>();
+        pickup = GetComponentInChildren<FootballPickup>();
+    }
+
+    public void ThrowFootball(Vector2 force)
+    {
+        pickup.throwTime = Time.time;
+        rbody.AddForce(force, ForceMode2D.Impulse);
+    }
+}
