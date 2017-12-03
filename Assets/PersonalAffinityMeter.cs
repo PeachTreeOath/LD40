9f3260490df32eec11ec45c3df74372db9660fc0
Fangor <rfonger@gmail.com>
2017-12-03 05:18:29 -0700

    Hardest affinity meters

--- Assets/PersonalAffinityMeter.cs
+++ Assets/PersonalAffinityMeter.cs
@@ -5,16 +5,23 @@ using UnityEngine.UI;
 
 public class PersonalAffinityMeter : MonoBehaviour {
 
-    RectMask2D mask;
+    SpriteMask mask;
     EnemyController enemyController;
+    RectTransform spriteRectTransform;
+    SpriteRenderer spriteRenderer;
 
     float originalY;
+    float originalHeight;
 
 	// Use this for initialization
 	void Start () {
-        mask = GetComponentInChildren<RectMask2D>();
+        mask = GetComponent<SpriteMask>();
+        spriteRectTransform = GetComponentInChildren<RectTransform>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         enemyController = GetComponentInParent<EnemyController>();
-        originalY = mask.rectTransform.rect.y;
+        originalY = spriteRectTransform.localPosition.y;
+        originalHeight = spriteRenderer.bounds.size.y;
+        
 
     }
 	
@@ -30,17 +37,31 @@ public class PersonalAffinityMeter : MonoBehaviour {
 
         float affinityPercent = affinity / maxAffinity;
 
-        Vector2 maskPos = mask.rectTransform.localPosition;
-        Rect maskRect = mask.rectTransform.rect;
-        float maskHeight = mask.rectTransform.rect.height;
+        Vector3 spritePos = spriteRectTransform.transform.localPosition;
 
         Debug.Log("=====MaskTest=====");
-        Debug.Log("origY: " + originalY);
-        Debug.Log("maskRect: " + maskRect.x + " " + maskRect.y + " " + maskRect.height + " " + maskRect.width);
-        mask.rectTransform.rect.Set(maskRect.x,originalY + .5f, maskRect.width, maskRect.height);
+        Debug.Log("origHeight: " + originalY);
+        spritePos.Set(spritePos.x, originalY - 1f + affinityPercent, spritePos.z);
 
-        Rect maskRect2 = mask.rectTransform.rect;
-        Debug.Log("maskRect2: " + maskRect2.x + " " + maskRect2.y + " " + maskRect2.height + " " + maskRect2.width);
+        spriteRectTransform.localPosition = spritePos;
+        //Rect spriteRect = spriteRectTransform.rect;
+        //spriteRect.y = originalY + originalHeight * .5f;
+
+        //spriteRectTransform.rect.Set(spriteRect.x, spriteRect.y, spriteRect.width, spriteRect.height);
+
+
+
+        //Vector2 maskPos = mask.rectTransform.localPosition;
+        //Rect maskRect = mask.rectTransform.rect;
+        //float maskHeight = mask.rectTransform.rect.height;
+
+        //Debug.Log("=====MaskTest=====");
+        //Debug.Log("origY: " + originalY);
+        //Debug.Log("maskRect: " + maskRect.x + " " + maskRect.y + " " + maskRect.height + " " + maskRect.width);
+        //mask.rectTransform.rect.Set(maskRect.x,originalY + .5f, maskRect.width, maskRect.height);
+
+        //Rect maskRect2 = mask.rectTransform.rect;
+        //Debug.Log("maskRect2: " + maskRect2.x + " " + maskRect2.y + " " + maskRect2.height + " " + maskRect2.width);
         //maskPos.y = maskHeight / 2f;
         //mask.transform.position = maskPos;
 
