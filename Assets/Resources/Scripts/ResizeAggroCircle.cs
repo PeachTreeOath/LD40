495752d76431a41df0da0a4e93e516cd5b342282
Gary Wong <gawong@microsoft.com>
2017-12-03 01:04:47 -0800

    Replace unity auto-generated code tabs with spaces

--- Assets/Resources/Scripts/ResizeAggroCircle.cs
+++ Assets/Resources/Scripts/ResizeAggroCircle.cs
@@ -13,10 +13,10 @@ public class ResizeAggroCircle : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 
         float affinity = 0;
         float radius = 0;
