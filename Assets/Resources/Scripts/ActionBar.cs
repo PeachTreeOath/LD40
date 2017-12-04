11129f53b65b1d1d3807e3061ed3cc42b96f44f8
PeachTreeOath <peachtreeoath@gmail.com>
2017-12-03 22:51:54 -0700

    Action bar

---
+++ Assets/Resources/Scripts/ActionBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionBar : Singleton<ActionBar> {
+
+    public Text txt;
+
+    public Image img;
+	public void Activate()
+    {
+        img.enabled = true;
+        txt.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        img.enabled = false;
+        txt.enabled = false;
+    }
+
+    public void ChangeText(string text)
+    {
+        txt.text = text;
+    }
+}
