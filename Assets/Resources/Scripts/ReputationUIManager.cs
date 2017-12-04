1771db37b16e4b8f5076302985484e38c18cf03e
PeachTreeOath <peachtreeoath@gmail.com>
2017-12-03 17:49:33 -0700

    Canvas

--- Assets/Resources/Scripts/ReputationUIManager.cs
+++ Assets/Resources/Scripts/ReputationUIManager.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReputationUIManager : MonoBehaviour {
+public class ReputationUIManager : Singleton<ReputationUIManager> {
 
     public RectTransform furryFill;
     public RectTransform skaterFill;
     public RectTransform jockFill;
 
+    public float maxWidth;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        maxWidth = furryFill.rect.width;
+    }
+
+    public void UpdateFill(int barNum, float ratio)
+    {
+
+        if(barNum == 0)
+        {
+            // furryFill. = new Vector3(position, furryFill.position.y, furryFill.position.z);
+            //furryFill.sizeDelta = 
+        }
+
+      //  if (barNum == 1)
+           // skaterFill.position = new Vector3(position, skaterFill.position.y, skaterFill.position.z);
+
+      //  if (barNum == 2)
+        //    jockFill.position = new Vector3(position, jockFill.position.y, jockFill.position.z);
+            
+    }
 }
