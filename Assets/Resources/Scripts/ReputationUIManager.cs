468b508f334e8a1dc87f70f90afc3b1e669d160a
PeachTreeOath <peachtreeoath@gmail.com>
2017-12-03 18:50:35 -0700

    Lighten gradient

--- Assets/Resources/Scripts/ReputationUIManager.cs
+++ Assets/Resources/Scripts/ReputationUIManager.cs
@@ -1,33 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReputationUIManager : Singleton<ReputationUIManager> {
 
-    public RectTransform furryFill;
-    public RectTransform skaterFill;
-    public RectTransform jockFill;
+    public Image furryFill;
+    public Image skaterFill;
+    public Image jockFill;
 
-    public float maxWidth;
+    public float minWidth = 0.07f;
     // Use this for initialization
     void Start() {
-        maxWidth = furryFill.rect.width;
+        
     }
 
     public void UpdateFill(int barNum, float ratio)
     {
+        float fillRatio = Mathf.Max(minWidth, ratio);
 
         if(barNum == 0)
         {
-            // furryFill. = new Vector3(position, furryFill.position.y, furryFill.position.z);
-            //furryFill.sizeDelta = 
+            furryFill.fillAmount = fillRatio;
         }
 
-      //  if (barNum == 1)
-           // skaterFill.position = new Vector3(position, skaterFill.position.y, skaterFill.position.z);
+        if (barNum == 1)
+            skaterFill.fillAmount = fillRatio;
 
-      //  if (barNum == 2)
-        //    jockFill.position = new Vector3(position, jockFill.position.y, jockFill.position.z);
-            
+
+        if (barNum == 2)
+            jockFill.fillAmount = fillRatio;
     }
 }
