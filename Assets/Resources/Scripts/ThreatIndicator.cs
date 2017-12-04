d8595735f78c2c0df60673fa1354d8419c72cafa
DarkFortress2\Elyk Yevrah <elykyevrah@gmail.com>
2017-12-03 18:47:59 -0600

    Added threat indicators.

---
+++ Assets/Resources/Scripts/ThreatIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreatIndicator : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(PlayerStateController.instance.CurrentState == transform.parent.GetComponent<EnemyController>().clique)
+        {
+            //Green
+            spriteRenderer.color = new Color(0.0f, 1.0f, 0.0f, 0.2f);
+            
+        }
+        else
+        {
+            //Red
+            spriteRenderer.color = new Color(1.0f, 0.0f, 0.0f, 0.2f);
+        }
+	}
+
+
+}
