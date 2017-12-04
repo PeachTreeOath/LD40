a8e204ffeaccdde038892bf7b6d46ba7fb4b9702
mjeffery <michael.jeffery@gmail.com>
2017-12-03 21:02:20 -0500

    a few more fixes for the nobody

--- Assets/Resources/Scripts/EnemyState/NobodyController.cs
+++ Assets/Resources/Scripts/EnemyState/NobodyController.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class NobodyController : WandererEnemyController {
 
     public override float GetStateSpeed() {
-        return 0.2f;
+        return 2f;
     }
 }
